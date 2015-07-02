0fee19f34fe7477a19407cd998a8e5784e7fed3c
lordjianqiu <392748697@qq.com>
2015-07-02 12:35:33 +0800

    添加音效

--- Assets/Script/GameUIScript.cs
+++ Assets/Script/GameUIScript.cs
@@ -4,33 +4,43 @@ using System.Collections;
 public class GameUIScript : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-	
-	}
+
 	public Transform pause;
 	public Transform stop;
 	public Transform died;
 	public bool isDied=false;
 	// Update is called once per frame
+	public AudioClip stopAudio;
+	public AudioClip btnAudio;
+	public AudioClip restartAudio;
+	//public Main main=new Main();
+	public Vector3 v3=new Vector3(0,0,0);
 
 	public void OnStopButtonClick(){
-
+		if(new Main().isSoundActivating)
+		AudioSource.PlayClipAtPoint (stopAudio, v3);
 		pause.gameObject.SetActive (true);
 		stop.gameObject.SetActive (false);
 		//游戏暂停代码
 	}
 
 	public void OnContinueButtonClick(){
+		if(new Main().isSoundActivating)
+		AudioSource.PlayClipAtPoint (btnAudio, v3);
 		pause.gameObject.SetActive (false);
 		stop.gameObject.SetActive (true);
 		print ("游戏继续代码");
 	}
 	public void OnBackButtonClick(){
+		if(new Main().isSoundActivating)
+		AudioSource.PlayClipAtPoint (btnAudio, v3);
 		pause.gameObject.SetActive (false);
 		stop.gameObject.SetActive (true);
 		print ("游戏后退代码");//后退代码
 	}
 	public void OnRestartButtonClick(){
+		if(new Main().isSoundActivating)
+		AudioSource.PlayClipAtPoint (restartAudio, v3);
 		pause.gameObject.SetActive (false);
 		stop.gameObject.SetActive (true);
 		print ("游戏重新开始");
--- Assets/Script/Main.cs
+++ Assets/Script/Main.cs
@@ -7,10 +7,16 @@ public class Main : MonoBehaviour {
 	//public Transform a;
 	//设置界面
 	public bool b=false;
+	public AudioClip btnAudio;
+	public Vector3 v3=new Vector3(0,0,0);
+	public bool isSoundActivating=true;
+	public bool isMusicActivating=true;
 	public void OnSettingBtnClick(Transform a){
+
 		Vector3 vec = a.transform.position;
 		if (!b) { 
-
+			if(isSoundActivating)
+				AudioSource.PlayClipAtPoint (btnAudio, v3);
 			vec.x += 68f;
 			print (vec);
 			LeanTween.move (a.gameObject, vec, 0.1f).setOnComplete (TweenOnCompleteTrue);
@@ -43,20 +49,39 @@ public class Main : MonoBehaviour {
 		}
 	}
 	public void PlayGame(){
+		if(isSoundActivating)
+			AudioSource.PlayClipAtPoint (btnAudio, v3);
 		print ("开始游戏");
-		//t.transform.GetComponent<Rigidbody> ().AddForce (Vector3.forward * 100000);
+
 	}
 	public void ShowGameCenter(){
+		if(isSoundActivating)
+			AudioSource.PlayClipAtPoint (btnAudio, v3);
 		print ("打开游戏中心");
 	}
 
 	public void SoundToggle(bool isOff){
+		if(isSoundActivating)
+			AudioSource.PlayClipAtPoint (btnAudio, v3);
 		print (isOff);
+		if (isOff)
+			isSoundActivating = false;
+		else
+			isSoundActivating = true;
 	}
 	public void MusicToggle(bool isOff){
+		if(isSoundActivating)
+			AudioSource.PlayClipAtPoint (btnAudio, v3);
 		print (isOff);
+		if (isOff)
+			isMusicActivating = false;
+		else
+			isMusicActivating = true;
+
 	}
 	public void DeleteAdvert(){
+		if(isSoundActivating)
+			AudioSource.PlayClipAtPoint (btnAudio, v3);
 		print ("DeleteAdvert");
 	}
 
