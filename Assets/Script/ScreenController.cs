dcc5fb5b50f71220bc75acde72996c944718982f
lordjianqiu <392748697@qq.com>
2015-07-07 11:45:19 +0800

    2015.7.7

---
+++ Assets/Script/ScreenController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenController : MonoBehaviour {
+
+	 public bool isFront=true;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//print (isFront);
+	}
+
+	public void SetAsFront (bool front) {
+		isFront = front;
+		Movement[] movs = gameObject.GetComponentsInChildren<Movement> ();
+		foreach (Movement mov in movs) {
+			if(mov.des!=null){
+				mov.des.toggle=isFront;
+				//print(front);
+			}
+		}
+	}
+}
